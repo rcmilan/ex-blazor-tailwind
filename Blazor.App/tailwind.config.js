0506fa399565eca7dc58e8b36863fa6e43a8cd99
Ricardo Milan <ricardo@intercambiodireto.com>
2022-02-03 09:21:44 -0300

    Add project files.

---
+++ Blazor.App/tailwind.config.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+    mode: 'jit',
+    purge: [
+        './wwwroot/index.html',
+        './**/*.razor',
+    ],
+    darkMode: 'media',
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: []
+}
